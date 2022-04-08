a2b7141071597a8203acc9aceb08b86df80d2dd9
东民 <xua744531854@163.com>
2022-04-08 21:12:19 +0800

    feature: cloner

---
+++ src/main/java/com/moonsky/mapper/annotation/Cloner.java
@@ -0,0 +1,18 @@
+package com.moonsky.mapper.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author benshaoye
+ */
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Cloner {
+
+    @Target({ElementType.FIELD})
+    @Retention(RetentionPolicy.SOURCE)
+    @interface Deep {}
+}
